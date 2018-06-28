83ad3977d48b5f428cdff3afdbd4ae5b588ad0e7
ase17_man <23743258+sylphair@users.noreply.github.com>
2018-06-28 23:36:40 +0800

    new web

--- src/main/java/com/example/demo/controller/Hello.java
+++ src/main/java/com/example/demo/controller/Hello.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Hello
 {
-    @RequestMapping("/")
+    @RequestMapping("/hello")
     public String sayHello()
     {
         return "Hello!DevOps.";
