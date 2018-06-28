11ff1186d90be99e42a38bb1e4498eea7f7ca217
ase17_man <23743258+sylphair@users.noreply.github.com>
2018-06-28 09:37:33 +0800

    DemoProj

---
+++ src/main/java/com/example/demo/DevOpsDemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevOpsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevOpsDemoApplication.class, args);
+	}
+}
--- src/main/java/com/example/demo/controller/Hello.java
+++ src/main/java/com/example/demo/controller/Hello.java
@@ -1,5 +1,14 @@
 package com.example.demo.Controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class Hello
 {
+    @RequestMapping("/")
+    public String sayHello()
+    {
+        return "Hello!";
+    }
 }
